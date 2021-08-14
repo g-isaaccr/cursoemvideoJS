9155c8bab830578cea7b6515a05b059d73800aa1
Gabriel <gabrielisaacosta@hotmail.com>
2021-08-14 10:58:56 -0300

    Adicionado exercicio para praticar

--- Aula14ex/ex016/script.js
+++ Aula14ex/ex016/script.js
@@ -5,13 +5,36 @@ function inicio(){
     var fim = window.document.getElementById('fim')
     var pass = window.document.getElementById('pass')
     var res = window.document.getElementById('res')
+   
+   if(ini.value.length == 0 || fim.value.length == 0 || pass.value.length == 0){
+       res.innerHTML = `Impossível contar!` 
 
-   if(ini.value.length == 0 || fim.value.length ==0 || pass.value.length == 0){
-       window.alert('erro')
    }
-   else if(pass == 0)
+   else 
     {
-        window.alert(`PTudo OK`)
-        pass = 1
+        res.innerHTML = `Contando: <br>`
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(pass.value)
+        if(p <= 0 ){
+            window.alert('Passo inválido! Considerando Passo 1')
+            p = 1
+        } 
+        
+        if(i<f){
+        for(var c = i;c<=f;c += p) // Contagem crescente
+            {
+            res.innerHTML += ` ${c} \u{1F449} `
+           
+            } 
+        
+        }
+        else{
+            for(var c = i; c>=f;c -= p)// Contagem decrescente
+            {
+            res.innerHTML += ` ${c} \u{1F449}`
+            }
+        } 
+            res.innerHTML += `\u{1F3C1}`
+        }
     }  
-}
