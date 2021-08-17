e5f18d1a9a7a6a97e24bff4eaf9d32dd4ef672de
Gabriel <gabrielisaacosta@hotmail.com>
2021-08-17 13:56:03 -0300

    Adicionado aula sobre funcoes

---
+++ Aula14ex/Aula15/funcao01.js
@@ -0,0 +1,10 @@
+function parimpar(n){
+    if(n%2 == 0){ //n%2 verificando se é par
+        return ` Par! `
+    }else {
+        return ` Impar! `
+
+    }
+}
+
+ console.log(parimpar(12)) 
---
+++ Aula14ex/Aula15/funcao02.js
@@ -0,0 +1,6 @@
+ function soma (n1=0, n2=0) { // Deixo os parâmetros iguais a 0, para caso eu não determine valor a eles eu considere o "0"
+    return n1 + n2
+
+ }
+
+ console.log(soma(5,9))
